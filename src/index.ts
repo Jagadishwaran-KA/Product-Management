d2f32b2d713595a50adebc117b7d1baa9f239fdd
JAGADISHWARAN-KA <jagadishwaranmass2003@gmail.com>
2024-10-13 17:15:23 +0530

    Refractored

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 require("dotenv").config();
 import prisma from "./db";
 import express, { Request, Response } from "express";
-import { validateId, validateScehma } from "./utils";
+import {
+  handleError,
+  validateId,
+  validateScehma,
+  StatusCode,
+  handleResponse,
+} from "./utils";
 
 export const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello :)");
-});
-
 app.get("/products", async (req: Request, res: Response) => {
   const data = await prisma.product.findMany();
-  res.json(data);
+  handleResponse(res, StatusCode.OK, data);
 });
 
 app.get("/products/:id", validateId, async (req: Request, res: Response) => {
@@ -23,29 +25,29 @@ app.get("/products/:id", validateId, async (req: Request, res: Response) => {
         id: Number(req.params.id),
       },
     });
-    res.status(200).json(data);
+    handleResponse(res, StatusCode.OK, data);
   } catch (error: any) {
-    if (error.code === "P2025") {
-      res
-        .status(400)
-        .json({ error: "No product found with id " + req.params.id });
-    } else {
-      res.status(500).send("Internal Server error " + error);
-    }
+    handleError(error, req, res);
   }
 });
 
-app.post("/products", validateScehma, async (req: Request, res: Response) => {
-  await prisma.product.create({
-    data: req.body,
-  });
-  res.status(201).json({ message: "Data has been Added successfully" });
-});
+app.post(
+  "/products",
+  validateScehma(false),
+  async (req: Request, res: Response) => {
+    await prisma.product.create({
+      data: req.body,
+    });
+    handleResponse(res, StatusCode.CREATED, {
+      message: "Data has been Added successfully",
+    });
+  }
+);
 
 app.put(
   "/products/:id",
   validateId,
-  validateScehma,
+  validateScehma(true),
   async (req: Request, res: Response) => {
     const id = Number(req.params.id);
     try {
@@ -56,13 +58,12 @@ app.put(
         data: req.body,
       });
 
-      res.status(200).json({ message: "Product has been updated", data });
+      handleResponse(res, StatusCode.OK, {
+        message: "Product has been updated",
+        data,
+      });
     } catch (error: any) {
-      if (error.code === "P2025") {
-        res.status(404).json({ error: `No product with the given id ${id}` });
-      } else {
-        res.status(500).send("Internal Server error " + error);
-      }
+      handleError(error, req, res);
     }
   }
 );
@@ -75,15 +76,12 @@ app.delete("/products/:id", validateId, async (req: Request, res: Response) => {
         id: id,
       },
     });
-    res.status(200).send(`Product with the id ${id} has been deleted`);
+
+    handleResponse(res, StatusCode.OK, {
+      message: `Product with the id ${id} has been deleted`,
+    });
   } catch (error: any) {
-    if (error.code === "P2025") {
-      res
-        .status(400)
-        .json({ error: "No product found with the given id " + id });
-    } else {
-      res.status(500).send("Internal Server error " + error);
-    }
+    handleError(error, req, res);
   }
 });
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
-import { app } from ".";
+
+export enum StatusCode {
+  OK = 200,
+  CREATED = 201,
+  NOTFOUND = 404,
+  SERVERERROR = 500,
+  PRISMA = "P2025",
+}
 
 const ProductSchema = z.object({
   name: z.string(),
@@ -11,23 +18,41 @@ const ProductSchema = z.object({
 const Id = z.coerce.number();
 
 export const validateId = (req: Request, res: Response, next: NextFunction) => {
-  const id = Id.safeParse(req.body.id);
+  const id = Id.safeParse(req.params.id);
   if (!id.success) {
-    res.status(400).json({ error: id.error.errors });
+    res.status(StatusCode.NOTFOUND).json({ error: id.error.errors });
   } else {
     next();
   }
 };
 
-export const validateScehma = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const data = ProductSchema.safeParse(req.body);
-  if (!data.success) {
-    res.status(400).json({ error: data.error.errors });
+export const validateScehma = (partial: boolean) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    var data;
+    if (partial) data = ProductSchema.partial().safeParse(req.body);
+    else data = ProductSchema.safeParse(req.body);
+    if (!data.success) {
+      res.status(StatusCode.NOTFOUND).json({ error: data.error.errors });
+    } else {
+      next();
+    }
+  };
+};
+
+export const handleError = (error: any, req: Request, res: Response) => {
+  if (error.code === StatusCode.PRISMA) {
+    res
+      .status(StatusCode.NOTFOUND)
+      .json({ error: "No product found with id " + req.params.id });
   } else {
-    next();
+    res.status(StatusCode.SERVERERROR).send("Internal Server error " + error);
   }
+};
+
+export const handleResponse = (
+  res: Response,
+  statuscode: number,
+  data: any
+) => {
+  res.status(statuscode).json(data);
 };
